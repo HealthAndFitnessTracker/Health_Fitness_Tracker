5487db655daf23997586c6686f15cfb3675c8a7c
Haimei <130692101+Hailey1107@users.noreply.github.com>
2023-04-30 21:33:56 +0200

    delete calculate

--- app.py
+++ app.py
@@ -18,13 +18,5 @@ def signUp():
 def signInCheck():
     return null
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    weight = float(request.form['weight'])
-    height = float(request.form['height']) / 100
-    bmi = weight / (height ** 2)
-    return render_template('bmi.html', bmi=bmi)
-
-
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
