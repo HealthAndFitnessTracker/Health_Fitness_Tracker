59a01bf89cbd1d9c0f7db4d30410b8f91113355a
Haimei <130692101+Hailey1107@users.noreply.github.com>
2023-05-02 01:21:15 +0200

    add signUp page

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from forms.sign_in_form import SignInForm
+from forms.sign_up_form import SignUpForm
 
 views = Blueprint(__name__, "views")
 
@@ -17,10 +18,13 @@ def signIn():
         return 'Hello'
     return render_template('signIn.html', form=sign_in_form)
 
-@views.route('/signUp')
+@views.route('/signUp', methods=['GET', 'POST'])
 def signUp():
-    return render_template('signUp.html')
+    sign_up_form = SignUpForm()
+    if sign_up_form.validate_on_submit():
+        name = sign_up_form.name.data
+        email = sign_up_form.email.data
+        password = sign_up_form.password.data
+        return 'Hello'
 
-@views.route('/signInCheck', methods=['POST'])
-def signInCheck():
-    return "temp"
+    return render_template('signUp.html', form=sign_up_form)
