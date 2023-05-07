448b75fe324c02c60e687f9fee9e986397c2daac
Haimei <130692101+Hailey1107@users.noreply.github.com>
2023-05-08 00:26:58 +0200

    HFT-14 add user database table (#10)

--- configs.py
+++ configs.py
@@ -1,3 +1,3 @@
 class Config:
     SECRET_KEY = 'my-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///myapp.db'
+    DATABASE_URL = 'postgres://rbstydqlceajzm:5d1c67294d8a93c866c8dd44db535ee01dc333778d4849dbd330b57c1ccce946@ec2-52-205-45-222.compute-1.amazonaws.com:5432/ddjfk3pcm4r9r9'
